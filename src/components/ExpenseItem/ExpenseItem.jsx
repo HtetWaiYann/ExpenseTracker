a5f3311bd421b0b2e04abab3975a4e92cd6af57b
Htet Wai Yan <hw.yan@mit.com.mm>
2023-01-28 14:17:43 +0630

    create expense component

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
+import './ExpenseItem.css'
+
 const ExpenseItem = () => {
   return (
-    <div>ExpenseItem</div>
+    <div className='expense-item'>
+        <div>January 28th 2023</div>
+        <div className='expense-item__description'>
+            <h2>Eating</h2>
+            <div className='expense-item__price'>5000 MMK</div>
+        </div>
+    </div>
   )
 }
 
